ba90474a9ec3495b59ae2ed58ffc9c2940e7484f
RakeshJakka5158 <rakeshjakka5158@gmail.com>
2024-05-31 14:53:24 +0530

    Initial commit

---
+++ src/main/java/Models/Student.java
@@ -0,0 +1,16 @@
+package Models;
+
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Student extends BaseModel{
+    private String name;
+    private int age;
+    private String mailID;
+    private String contactNumber;
+
+}
